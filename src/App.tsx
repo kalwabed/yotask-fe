7bbde19870cf08982d6e7c825c87d13ddd28607d
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-02-16 20:49:12 +0700

    feat: initial ui

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { Button, Container, Flex, FormControl, FormErrorMessage, FormLabel, Heading, Input, Text } from "@chakra-ui/react"
+import ky from 'ky'
+import { useForm } from "react-hook-form"
+
+function App() {
+  const { register, handleSubmit, formState: { errors }} = useForm<{ username: string; password: string }>()
+
+  const login = async (data: { username: string; password: string }) => {
+    try {
+    const userLogin = await ky.post('http://localhost:8000/auth/login', {
+      json: {
+        username: data.username,
+        password: data.password
+      }
+    }).json()
+
+    console.log(userLogin)
+    } catch(err) {
+      console.error(err)
+    }
+  }
+  return (
+    <Flex as="main" w='100%' direction='column' h='100%'>
+      <Container mt={12}>
+        <Flex direction="column" gap={4}>
+          <Heading as="h1">
+            Welcome to YoTask
+          </Heading>
+          <Text>Please login to proceed</Text>
+          <Flex as="form" onSubmit={handleSubmit(login)} direction="column" gap={8}>
+            <FormControl isInvalid={Boolean(errors.username)}>
+              <FormLabel>Username</FormLabel>
+              <Input {...register('username', { required: true})} />
+              {errors.username && 
+              <FormErrorMessage>Username is required.</FormErrorMessage>}
+            </FormControl>
+            <FormControl isInvalid={Boolean(errors.password)}>
+              <FormLabel>Password</FormLabel>
+              <Input type="password" {...register('password', { required: true })} />
+              {errors.password && 
+              <FormErrorMessage>Password is required.</FormErrorMessage>}
+            </FormControl>
+            <Button type="submit" colorScheme="teal">Login</Button>
+          </Flex>
+        </Flex>
+      </Container>
+    </Flex>
+  )
+}
+
+export default App
